546616508a5947d3c321a1437255fede3747e13c
Asad <bonjourdofus1@gmail.com>
2022-07-15 21:46:22 +0100

    Add and Config Mongoose

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const api = require('./routes/api');
 const app = express();
 const port = 6969;
 
+
 app.use("/api", api);
 
 
-app.listen(port, () => {
+app.listen(port, async() => {
+
+   await mongoose.connect('mongodb://localhost:27017/arbitrage',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
     console.log(`Sever listening on : ${port} `)
 })
--- routes/api.js
+++ routes/api.js
@@ -1,37 +1,7 @@
 var express = require('express');
-
-const { MongoClient } = require('mongodb');
-
-const mongoClient = new MongoClient(url);
-
+const mongoose = require('mongoose');
 const router = express.Router();
-
-var data = [
-    {
-        "blockNumber": "15376048",
-        "pair": "AXS-ETH",
-        "side": "BUY",
-        "amountIn": "114693",
-        "amountOutMin": "391396677508131098",
-        "amountOut": "391844932944906372"
-    },
-    {
-        "blockNumber": "15376048",
-        "pair": "SLP-ETH",
-        "side": "BUY",
-        "amountIn": "114693",
-        "amountOutMin": "391396677508131098",
-        "amountOut": "391844932944906372"
-    },
-    {
-        "blockNumber": "15376048",
-        "pair": "BNB-ETH",
-        "side": "BUY",
-        "amountIn": "114693",
-        "amountOutMin": "391396677508131098",
-        "amountOut": "391844932944906372"
-    }
-]
+const Trade = require("../models/trade");
 
 router.get('/trades', (req, res) => {
 
@@ -39,7 +9,13 @@ router.get('/trades', (req, res) => {
 
     res.header('Access-Control-Allow-Origin', '*');
 
-    res.json({ data: data.slice(0, limit) });
+    Trade.find({}, (err, trades) => {
+
+        res.json({ trades });
+        
+    })
+
+   
 });
 
 module.exports = router;
